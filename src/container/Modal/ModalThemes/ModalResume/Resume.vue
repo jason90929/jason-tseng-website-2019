b18e5ff1c2b93a5eb47dcbe821ee7156c08b0593
Jason Tseng <tsaistorm@gmail.com>
2018-09-25 17:27:42 +0800

    Add fonts files with css

--- src/container/Modal/ModalThemes/ModalResume/Resume.vue
+++ src/container/Modal/ModalThemes/ModalResume/Resume.vue
@@ -1,49 +1,54 @@
 <template>
   <article class="resume">
-    <h1>Jason Tseng - Frontend Developer</h1>
-    <h2>Education</h2>
+    <div class="resume-header">
+      <h1>Jason Tseng</h1>
+      <h2>Frontend Developer</h2>
+    </div>
+    <h3>Education</h3>
     <p>
       National Taichung University of Science and Technology
     </p>
     <p>
       Computer Science 2009-2013
     </p>
-    <h2>
+    <h3>
       Skills
-    </h2>
+    </h3>
     <pre>
       JavaScript Framework
-      ★★★★★Vue2
-      ★★★☆☆React
-      ★☆☆☆☆Angular1
-      ★☆☆☆☆EmberJS
+      Vue2
+      React
       HTML5
-      ★★★★☆SEO
+
       CSS3
-      ★★★★★sass/scss, stylus
+      sass/scss
+      stylus
+
       VR Framework
-      ★★★★★Krpano
-      ★★★★☆A-Frame
-      ★★★☆☆Three.js
+      Krpano
+      A-Frame
+      Three.js
+
       Server
       NodeJS, Apache, Nginx, Linux, AWS, GCP, Firebase, 3rd Party API, Restful API
+
       Others
       Git, Sketch, Emmet
     </pre>
 
-    <h2>Experience</h2>
+    <h3>Experience</h3>
 
-    <p>2017/02 - present</p>
-    <h3>iStaging</h3>
-    <h4>VR/AR for B2B2C solution</h4>
+    <h6>2017/02 - present</h6>
+    <h4>iStaging</h4>
+    <h5>VR/AR for B2B2C solution</h5>
 
-    <p>2016/04 - 2017/01</p>
-    <h3>Nong Design</h3>
-    <h4>Case jobs for Wordpress website</h4>
+    <h6>2016/04 - 2017/01</h6>
+    <h4>Nong Design</h4>
+    <h5>Case jobs for Wordpress website</h5>
 
-    <p>2014/08 - 2016/03</p>
-    <h3>Wethink</h3>
-    <h4>Case jobs for Software assistant and human resource</h4>
+    <h6>2014/08 - 2016/03</h6>
+    <h4>Wethink</h4>
+    <h5>Case jobs for Software assistant and human resource</h5>
   </article>
 </template>
 
@@ -54,4 +59,14 @@ export default {
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
+@import '../../../../stylesheets/variables';
+
+.resume {
+
+}
+
+.resume-header {
+  background-color: $orange;
+  color: $white;
+}
 </style>
