6178e1edc4cf553fd2009e20ecbf449299cce893
Jason Tseng <tsaistorm@gmail.com>
2018-09-25 16:16:03 +0800

    Add some text

--- src/container/Modal/ModalThemes/ModalResume/Resume.vue
+++ src/container/Modal/ModalThemes/ModalResume/Resume.vue
@@ -1,7 +1,50 @@
 <template>
-  <div class="resume">
-    Resume
-  </div>
+  <article class="resume">
+    <h1>Jason Tseng - Frontend Developer</h1>
+    <h2>Education</h2>
+    <p>
+      National Taichung University of Science and Technology
+    </p>
+    <p>
+      Computer Science 2009-2013
+    </p>
+    <h2>
+      Skills
+    </h2>
+    <pre>
+      JavaScript Framework
+      ★★★★★Vue2
+      ★★★☆☆React
+      ★☆☆☆☆Angular1
+      ★☆☆☆☆EmberJS
+      HTML5
+      ★★★★☆SEO
+      CSS3
+      ★★★★★sass/scss, stylus
+      VR Framework
+      ★★★★★Krpano
+      ★★★★☆A-Frame
+      ★★★☆☆Three.js
+      Server
+      NodeJS, Apache, Nginx, Linux, AWS, GCP, Firebase, 3rd Party API, Restful API
+      Others
+      Git, Sketch, Emmet
+    </pre>
+
+    <h2>Experience</h2>
+
+    <p>2017/02 - present</p>
+    <h3>iStaging</h3>
+    <h4>VR/AR for B2B2C solution</h4>
+
+    <p>2016/04 - 2017/01</p>
+    <h3>Nong Design</h3>
+    <h4>Case jobs for Wordpress website</h4>
+
+    <p>2014/08 - 2016/03</p>
+    <h3>Wethink</h3>
+    <h4>Case jobs for Software assistant and human resource</h4>
+  </article>
 </template>
 
 <script>
