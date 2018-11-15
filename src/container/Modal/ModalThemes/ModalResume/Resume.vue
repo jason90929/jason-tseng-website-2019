478d079abd39fc067e88163dbd7ac1bad6bea222
Jason Tseng <tsaistorm@gmail.com>
2018-11-15 16:44:57 +0800

    Add some project experience

--- src/container/Modal/ModalThemes/ModalResume/Resume.vue
+++ src/container/Modal/ModalThemes/ModalResume/Resume.vue
@@ -42,12 +42,17 @@
       <h3>Experience</h3>
       <br>
       <h6 class="oswald-light">2017/02 - present</h6>
-      <h4>iStaging</h4>
-      <p>VR/AR for B2B2C solution</p>
+      <h4>iStaging - VR/AR for B2B2C solution</h4>
+      <p>Handle a project TourRing with 90% code, based on AFrame, Vue2, Three.js, Stylus, Firebase, Agora(WebRTC SDK)</p>
+      <p>Handle a project LiveTour with 85% code, based on Krpano, Vue2, Three.js, Stylus, Firebase, NodeJS</p>
+      <p>Handle a project VREditor with 50% code, based on Vue2, Three.js, Stylus, Firebase</p>
       <br>
       <h6 class="oswald-light">2016/04 - 2017/01</h6>
-      <h4>Nong Design</h4>
-      <p>Case jobs for Wordpress website</p>
+      <h4>Nong Design - Case jobs for Wordpress website</h4>
+      <p>Complete 9 projects in the company (I stay in Nong Design 9 months)</p>
+      <p>Use Gulp, HTML, SCSS structure to complete static website</p>
+      <p>Then process with PHP code to build a WordPress website</p>
+      <p>Do lots of CSS animation on websites</p>
       <br>
       <h6 class="oswald-light">2014/08 - 2016/03</h6>
       <h4>Wethink</h4>
