651565655ea4fdf586174fb924d66a7146db3808
Jason Tseng <tsaistorm@gmail.com>
2018-12-21 10:24:07 +0800

    Create modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './modal.scss'
+
+class Modal extends Component {
+  static defaultProps = {
+    closable: PropTypes.bool,
+    children: PropTypes.node
+  }
+
+  constructor () {
+    super()
+    this.state = {
+      active: false
+    }
+  }
+
+  componentDidMount () {
+    window.addEventListener('keydown', this.modalKeydown)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('keydown', this.modalKeydown)
+  }
+
+  modalKeydown (e) {
+    if (e.keyCode === 27) { // 27 = esc
+      this.closeModal()
+    }
+  }
+
+  closeModal () {
+    this.setState(prevState => ({
+      active: false
+    }))
+  }
+
+  render () {
+    return (
+      <div
+        className="modal"
+        onClick={this.props.closable ? this.closeModal : null}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default Modal
