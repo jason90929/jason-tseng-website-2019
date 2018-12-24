a56beea4a28f35e3c4976ab04d29e304f06145a0
Jason Tseng <tsaistorm@gmail.com>
2018-12-24 12:04:04 +0800

    Refactor portfolio

--- src/pages/Home/ThirdArea.js
+++ src/pages/Home/ThirdArea.js
@@ -11,101 +11,60 @@ import homeIntegrationImage from '../../assets/images/portfolio/Home-Integration
 import './third-area.scss'
 
 class ThirdArea extends Component {
+  state = {
+    portfolio: [{
+      text: 'iStaging',
+      title: 'TourRing',
+      image: tourRingImage,
+      color: '#BBEA86'
+    }, {
+      text: 'iStaging',
+      title: '3D DollHouse',
+      image: threeDDollHouseImage,
+      color: '#dac9ff'
+    }, {
+      text: 'Nong Design',
+      title: 'Nong Design',
+      image: nongDesignImage,
+      color: '#ffe500'
+    }, {
+      text: 'Nong Design',
+      title: 'CNEWS',
+      image: cnewsImage,
+      color: '#21D8DA'
+    }, {
+      text: 'Nong Design',
+      title: 'Home Integration',
+      image: homeIntegrationImage,
+      color: '#acacac'
+    }]
+  }
+
   render () {
     const startFrom = this.props.pageList.findIndex(page => page === 'portfolio')
     const shouldLoaded = this.props.isLoaded && this.props.pageList[this.props.currentPage] === 'portfolio'
-    const className = cx('third-area', {
-      'area-loaded': shouldLoaded && this.props.currentPage - startFrom === 0
+    return this.state.portfolio.map((portfolio, index) => {
+      const className = cx('third-area', {
+        'area-loaded': shouldLoaded && this.props.currentPage - startFrom === index
+      })
+      return (
+        <Area
+          key={`ThirdArea${index}`}
+          className={className}>
+          <Portfolio
+            text={portfolio.text}
+            title={portfolio.title}
+            image={portfolio.image}
+            color={portfolio.color}
+            textClass="third-area-frame-text"
+            titleClass="third-area-frame-title"
+            btnClass="third-area-frame-btn"
+            imageClass="third-area-frame-image"
+            colorClass="third-area-frame-color">
+          </Portfolio>
+        </Area>
+      )
     })
-    const className1 = cx('third-area', {
-      'area-loaded': shouldLoaded && this.props.currentPage - startFrom === 1
-    })
-    const className2 = cx('third-area', {
-      'area-loaded': shouldLoaded && this.props.currentPage - startFrom === 2
-    })
-    const className3 = cx('third-area', {
-      'area-loaded': shouldLoaded && this.props.currentPage - startFrom === 3
-    })
-    const className4 = cx('third-area', {
-      'area-loaded': shouldLoaded && this.props.currentPage - startFrom === 4
-    })
-    return [
-      <Area
-        key="ThirdArea1"
-        className={className}>
-        <Portfolio
-          text="iStaging"
-          title="TourRing"
-          image={tourRingImage}
-          color="#BBEA86"
-          textClass="third-area-frame-text"
-          titleClass="third-area-frame-title"
-          btnClass="third-area-frame-btn"
-          imageClass="third-area-frame-image"
-          colorClass="third-area-frame-color">
-        </Portfolio>
-      </Area>,
-      <Area
-        key="ThirdArea2"
-        className={className1}>
-        <Portfolio
-          text="iStaging"
-          title="3D DollHouse"
-          image={threeDDollHouseImage}
-          color="#dac9ff"
-          textClass="third-area-frame-text"
-          titleClass="third-area-frame-title"
-          btnClass="third-area-frame-btn"
-          imageClass="third-area-frame-image"
-          colorClass="third-area-frame-color">
-        </Portfolio>
-      </Area>,
-      <Area
-        key="ThirdArea3"
-        className={className2}>
-        <Portfolio
-          text="Nong Design"
-          title="Nong Design"
-          image={nongDesignImage}
-          color="#ffe500"
-          textClass="third-area-frame-text"
-          titleClass="third-area-frame-title"
-          btnClass="third-area-frame-btn"
-          imageClass="third-area-frame-image"
-          colorClass="third-area-frame-color">
-        </Portfolio>
-      </Area>,
-      <Area
-        key="ThirdArea4"
-        className={className3}>
-        <Portfolio
-          text="Nong Design"
-          title="CNEWS"
-          image={cnewsImage}
-          color="#21D8DA"
-          textClass="third-area-frame-text"
-          titleClass="third-area-frame-title"
-          btnClass="third-area-frame-btn"
-          imageClass="third-area-frame-image"
-          colorClass="third-area-frame-color">
-        </Portfolio>
-      </Area>,
-      <Area
-        key="ThirdArea5"
-        className={className4}>
-        <Portfolio
-          text="Nong Design"
-          title="Home Integration"
-          image={homeIntegrationImage}
-          color="#acacac"
-          textClass="third-area-frame-text"
-          titleClass="third-area-frame-title"
-          btnClass="third-area-frame-btn"
-          imageClass="third-area-frame-image"
-          colorClass="third-area-frame-color">
-        </Portfolio>
-      </Area>
-    ]
   }
 }
 
