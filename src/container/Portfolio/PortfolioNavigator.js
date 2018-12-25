63b3c53c146da38cb8ae458ebb998025d92b6805
Jason Tseng <tsaistorm@gmail.com>
2018-12-25 15:29:05 +0800

    Improve portfolio navigator animation

--- src/container/Portfolio/PortfolioNavigator.js
+++ src/container/Portfolio/PortfolioNavigator.js
@@ -9,22 +9,28 @@ class PortfolioNavigator extends Component {
     const className = cx('portfolio-navigator', {
       'portfolio-navigator-active': this.props.pageList[this.props.currentPage] === 'portfolio'
     })
-    let translateY = `-${(this.props.currentPage - startFrom) * 20}%`
+    let translateY = `-${(this.props.currentPage - startFrom) * 1.4}em` // same as $size in scss file
     return (
       <div className={className}>
-        <span className="current-count-wrapper">
-          <span
-            className="current-count-inner"
-            style={{ transform: `translateY(${translateY})` }}>
-            <span className="current-count">1</span>
-            <span className="current-count">2</span>
-            <span className="current-count">3</span>
-            <span className="current-count">4</span>
-            <span className="current-count">5</span>
-          </span>
-        </span>
-        <span className="divider" />
-        <span className="portfolio-count">5</span>
+        <div className="current-count-wrapper">
+          <div className="current-count-inner">
+            <div
+              className="current-count-inside"
+              style={{ transform: `translateY(${translateY})` }}>
+              <span className="current-count">1</span>
+              <span className="current-count">2</span>
+              <span className="current-count">3</span>
+              <span className="current-count">4</span>
+              <span className="current-count">5</span>
+            </div>
+          </div>
+        </div>
+        <div className="divider" />
+        <div className="portfolio-count">
+          <div className="portfolio-count-inner">
+            5
+          </div>
+        </div>
       </div>
     )
   }
