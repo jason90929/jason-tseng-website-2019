7cf162b171aee2cd6965d07016c99f5e9c5662d9
Jason Tseng <tsaistorm@gmail.com>
2018-12-27 09:47:30 +0800

    Remove constructor

--- src/container/AppComponent.js
+++ src/container/AppComponent.js
@@ -2,10 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class AppComponent extends React.Component {
-  constructor (props, context) {
-    super(props, context)
-  }
-
   render () {
     return this.props.children
   }
