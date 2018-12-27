67e6a9b5eeab4d5bdab5b7ad5769be03a163f9a4
Jason Tseng <tsaistorm@gmail.com>
2018-12-27 11:02:27 +0800

    add some props for btn

--- src/components/Btn/Btn.js
+++ src/components/Btn/Btn.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import cx from 'classnames'
 import './btn.scss'
 
 class Btn extends Component {
   static defaultProps = {
     className: '',
+    type: 'button', // button, submit
     onClick: () => {},
+    disabled: false,
     children: null
   }
 
   static propTypes = {
     className: PropTypes.string,
+    type: PropTypes.string,
     onClick: PropTypes.func,
+    disabled: PropTypes.bool,
     children: PropTypes.node
   }
 
   render () {
+    const className = cx('btn', {
+      [this.props.className]: this.props.className
+    })
     return (
       <button
-        className={`btn ${this.props.className}`}
+        type={this.props.type}
+        className={className}
+        disabled={this.props.disabled}
         onClick={this.props.onClick}>
         {this.props.children}
       </button>
